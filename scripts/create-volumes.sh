f205680a155eb059793709e661c0a6a83ed2137c
Martin Paul <mortib20@gmail.com>
2023-02-18 16:47:47 +0100

    added roundcube and redone env file and other stuff

--- scripts/create-volumes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Create caddy_config"
-docker volume create caddy_config
-echo "Create caddy_data"
-docker volume create caddy_data
--- scripts/remove-volumes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Remove caddy_config"
-docker volume rm caddy_config
-echo "Remove caddy_data"
-docker volume rm caddy_data
